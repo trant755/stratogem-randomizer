2afddc05d42db3c018f8670dfae62025f8081dcd
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2024-06-21 18:58:22 +0300

    mk2 remove repeat

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const randomizerHendler = document.querySelector(".randomizer-hendler");
 const stratogemList = document.querySelector(".stratogem-list");
 const stratogemItem = document.querySelector(".stratogem-item");
 
-const markup = (imgNum) => `
+const markup = (imgNumArr) =>
+  imgNumArr.map(
+    (imgNum) => `
 <li class="stratogem-item">
     <img src="./img/${imgNum}.png" alt="stratogem" class="stratogem-item">
-</li>
-    `;
+</li>`
+  );
 
 let stratogem = [];
 randomizerHendler.addEventListener("click", () => {
@@ -17,8 +19,14 @@ randomizerHendler.addEventListener("click", () => {
   stratogem = [];
   for (let i = 0; i < 4; i++) {
     const random = Math.floor(Math.random() * 53) + 1;
-    stratogem.push(markup(random));
+    if (stratogem.includes(random)) {
+      i--;
+      continue;
+    }
+    stratogem.push(random);
   }
 
-  stratogemList.innerHTML = stratogem.join("");
+  const stratogemArr = markup(stratogem);
+
+  stratogemList.innerHTML = stratogemArr.join("");
 });
